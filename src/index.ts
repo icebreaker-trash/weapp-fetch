1ff99b2960b3842dd0778dcface7de0ee2fd72af
SonOfMagic <qq1324318532@gmail.com>
2023-03-08 11:18:34 +0800

    feat: commit simple request

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,130 @@ import { Headers, Request, Response, normalizeValue } from './fetch'
 import { defu } from 'defu'
 import { fixUrl, isPlainObject } from './util'
 import { UserDefinedOptions } from './type'
+
 // declare function fetch(
 //   input: RequestInfo | URL,
 //   init?: RequestInit
 // ): Promise<Response>
+
+// readonly headers: Headers;
+//     readonly ok: boolean;
+//     readonly redirected: boolean;
+//     readonly status: number;
+//     readonly statusText: string;
+//     readonly type: ResponseType;
+//     readonly url: string;
+//     clone(): Response;
+
+// type OrignResponse = typeof window.Response
+
+interface OrginResponse extends Body {
+  readonly headers: Headers
+  readonly ok: boolean
+  readonly redirected: boolean
+  readonly status: number
+  readonly statusText: string
+  readonly type: ResponseType
+  readonly url: string
+  clone(): OrginResponse
+}
+
+type SimpleResponse = {
+  cookies: string[]
+  profile: WechatMiniprogram.RequestProfile
+} & OrginResponse
+
+function makeSimpleResponse(
+  response: WechatMiniprogram.RequestSuccessCallbackResult<
+    string | WechatMiniprogram.IAnyObject | ArrayBuffer
+  >,
+  options: UserDefinedOptions
+): SimpleResponse {
+  return {
+    cookies: response.cookies,
+    profile: response.profile,
+    ok: ((response.statusCode / 100) | 0) === 2, // 200-299
+    statusText: response.errMsg,
+    status: response.statusCode,
+    url: response.header['X-Request-URL'] || options.url,
+    text: () => Promise.resolve(JSON.stringify(response.data)),
+    json: () => Promise.resolve(response.data),
+
+    clone: function () {
+      return this
+    },
+    // @ts-ignore
+    headers: new Headers(response.header),
+    /**
+     * @deprecated start
+     */
+    blob: () => Promise.reject(new Error('weapp has no blob object!')),
+    body: null,
+    bodyUsed: false,
+    type: 'basic',
+    redirected: false,
+    formData: () => Promise.reject(new Error('weapp has no formData object!')),
+    // 其实是有的
+    arrayBuffer: () =>
+      Promise.reject(new Error('weapp has no arrayBuffer object!'))
+
+    /**
+     * @deprecated end
+     */
+  }
+}
+
+export function createSimpleFetch(
+  requestFn: typeof wx.request,
+  defaults?: UserDefinedOptions
+) {
+  return function simpleFetch(url: string, options: UserDefinedOptions = {}) {
+    const {
+      enableCache,
+      enableChunked,
+      enableHttp2,
+      enableHttpDNS,
+      enableQuic,
+      forceCellularNetwork,
+      httpDNSServiceId,
+      timeout,
+      complete,
+      getTask,
+      headers,
+      method,
+      body,
+      dataType
+    } = defu(options, defaults)
+    const promise = new Promise((resolve, reject) => {
+      const innerOptions: WechatMiniprogram.RequestOption = {
+        url,
+        header: headers,
+        method: method || 'GET',
+        // @ts-ignore
+        data: body,
+        dataType,
+        // dataType:options.d
+        fail: reject,
+        success(response) {
+          resolve(makeSimpleResponse(response, innerOptions))
+        },
+        responseType: options.responseType,
+        enableCache,
+        enableChunked,
+        enableHttp2,
+        enableHttpDNS,
+        enableQuic,
+        forceCellularNetwork,
+        httpDNSServiceId,
+        timeout,
+        complete
+      }
+      const task = requestFn(innerOptions)
+      getTask?.(task, innerOptions)
+    })
+    return promise
+  }
+}
 
 function createFetch(
   requestFn: typeof wx.request,
@@ -35,7 +155,7 @@ function createFetch(
               headers: result.header, // parseHeaders(result.header || '')
               url: undefined
             }
-            options.url = options.headers['X-Request-URL'] || url
+            options.url = result.header['X-Request-URL'] || url
 
             let body = result.data
 
@@ -48,7 +168,7 @@ function createFetch(
               resolve(new Response(body, options))
             }, 0)
           },
-          method: request.method,
+          method: request.method || 'GET',
           fail(err) {
             setTimeout(function () {
               reject(err)
